00e83734ad063db79fd88e0d2d34522e38f6d5bc
Jun Zhao <zhaojunwater@gmail.com>
2019-02-20 22:56:57 -0500

    This repo is a collection of tools for lab experiment data organization and analysis.

---
+++ mate.py
@@ -0,0 +1,62 @@
+import psycopg2
+import argparse
+
+parser = argparse.ArgumentParser(description='Create a mate record for table transgenic_mouse_breeding.')
+parser.add_argument('--dad', required=True, help='Father id')
+parser.add_argument('--mom', required=True, help='Mother id')
+parser.add_argument('--cageid', required=True, help='Cage id')
+parser.add_argument('--server', default='localhost', help='Server host')
+parser.add_argument('--database', default='levy_lab_data', help='database name')
+parser.add_argument('--user', default='postgres', help='username')
+parser.add_argument('--password', default = None, help='password')
+
+args = parser.parse_args()
+
+def matestyle(num):
+    style = 'M' + '%04d' % (num)
+    return(style)
+
+def main(fatherid = args.dad, motherid = args.mom, cageid = args.cageid, server = args.server, 
+         database = args.database, user = args.user, password = args.password):
+    conncommend = "host={} dbname={} user={}".format(server, database, user)
+    # print(conncommend)
+    conn = psycopg2.connect(conncommend)
+
+    cur = conn.cursor()
+    cur.execute(
+    """
+    SELECT mateid FROM {}
+    """.format('transgenic_mouse_breeding')
+    )
+    mateids = cur.fetchall()
+    conn.commit()
+
+    mateids = [x[0] for x in mateids]
+    mateids.sort()
+
+    newmate = matestyle(int(mateids[-1][1:])+1)
+    
+    cur = conn.cursor()
+    cur.execute("INSERT INTO transgenic_mouse_breeding (mateid, cageid, fathter_id, mother_id, pair_date) VALUES ({}, {}, {}, {}, current_date);", 
+                (newmate, cageid, fatherid, motherid)
+    )
+    conn.commit()
+    
+    print('input done')
+
+    cur = conn.cursor()
+    cur.execute(
+    """
+    SELECT * FROM transgenic_mouse_breeding
+    WHERE mateid = {}
+    
+    """.format(newmate)
+    )
+    tmp = cur.fetchall()
+    conn.commit()
+    print(tmp)
+
+
+
+if __name__ == "__main__":
+    main()
