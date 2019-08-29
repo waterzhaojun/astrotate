6be3202a24ea5c81052eb543f86aeba64d4b81bc
Zhao <jzhao1@its.caregroup.org>
2019-08-29 13:51:22 -0400

    update

--- utils.py
+++ utils.py
@@ -12,17 +12,26 @@ from datetime import datetime
 import numpy as np
 import yaml
 from shutil import copyfile
+import importlib
 
 
 def readjson(path):
+    # read a json file
     with open(path, 'r') as json_file:
         data = json.load(json_file)
     return(data)
 
 
 def writejson(path, data):
+    # write a dict to a json file
     with open(path, 'w') as f:
         json.dump(data, f, indent=4)
+
+def update():
+    # As this module is frequently updated, this function helps you update the module
+    # This function is still under development. Not usable.
+    # importlib.reload(astrotate)
+    pass
 
 def format_date(string, outputformat = '%m-%d-%Y'):
     # the correct sequence should be month/day/year, otherwise there might be some error
@@ -43,6 +52,9 @@ def format_date(string, outputformat = '%m-%d-%Y'):
     return(strdate)
 
 def select(hellowords, array, **kwargs):
+    # This function helps you use input function to select a key value from an array.
+    # It list the elements and use choose the element by idx. Based on the idx, the 
+    # function return the value. if you want to add some extra words, use extra_note = 'xxx'.
     print(hellowords)
     for i in range(len(array)):
         print('%d ---> %s' % (i, array[i]))
@@ -55,6 +67,10 @@ def select(hellowords, array, **kwargs):
     return(final)
 
 def select_from_dict(hellowords, dict, **kwargs):
+    # This function helps you use input function to select a key value from a dict.
+    # The input should be a dict. The function list all the 'key: value' out. 
+    # If you want to return the key instead of value, use return_key = True.
+    # If you want multiple choice and return an array, use allow_array = True.
     print(hellowords)
     for key, value in dict.items():
         print('%s ---> %s' % (str(key), str(value)))
@@ -91,16 +107,25 @@ def projectArrayInput():
 # ====================================================================================================
 # ===== config of the system =========================================================================
 # ====================================================================================================
+# config file is important for the database setup. This file exist in the root folder of the module.
+# To update the config file, you can download the config file by using download_config function.
+# After revise, use upload_config file to overwrite the old one. Keep the one you used for update as 
+# every time after update the module, you have to upload the config again.
 def load_config():
+    # load config file. The config file can be used for setting up.
     with open('config.yml', 'r') as f:
         config = yaml.safe_load(f)
     return(config)
 
 def check_config():
+    # Check your config file
     print(load_config())
 
 def download_config(path):
+    # Download the config file from the module root folder
     copyfile('config.yml', path)
 
 def upload_config(path):
+    # upload the config file to module root folder.
+    # the path is where your file locally is.
     copyfile(path, 'config.yml')
