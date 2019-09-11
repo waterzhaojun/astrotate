f6d10b4a6e0ceb6d2878e0ac904ad66dbb50706a
Jun Zhao <zhaojunwater@gmail.com>
2019-09-11 01:22:19 -0400

    update

--- ihc.py
+++ ihc.py
@@ -8,6 +8,31 @@ import os
 
 import pandas as pd
 import numpy as np
-from astrotate import array, utils, treatment
+from astrotate import array, utils, treatment, surgery, config as cg
 import json
 
+
+class Exp(cg.Experiment):
+    def __init__(self, config):
+        super().__init__(config)
+        animallist = config.check_surgery_animal()
+        self.animalid = input('animal id: ')
+        if self.animalid not in animallist:
+            raise Exception('The animal id should be in the SURG folder first. Please confirm.')
+        
+        tmp = input('Staining date. Use the date to start primary staining.')
+        self.date = utils.format_date(datetime.strptime(tmp, '%y%m%d').strftime('%m/%d/%Y'))
+        self.mainfolder = os.path.join(self.root, config.system_path['twophoton'])
+
+        self.animalfolder = os.path.join(self.mainfolder, animalid)
+        if not os.path.exists(self.animalfolder):
+            os.mkdir(self.animalfolder)
+
+        self.expfile = os.path.join(self.animalfolder, str(dateid)+'.json')
+        if not os.path.exists(self.expfile):
+            exp = {}
+            exp['project'] = utils.projectArrayInput(config)
+            exp['treatment'] = {}
+            exp['data'] = []
+            utils.writejson(self.expfile, exp)
+        self.loadExp()
